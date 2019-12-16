79172c9e8a503cc8f3b6fa53f0f700146e449375
Dawid Sikora <dawid.sikora@ftsgps.com>
2019-12-16 11:54:34 +0100

    Day2Part1

---
+++ AdventOfCode2019/Day2/Day2Part1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using HelpingLibrary;
+
+namespace AdventOfCode2019.Day2
+{
+    public class Day2Part1
+    {
+        public static string ProcessCode(string opCode)
+        {
+            var split = opCode.Split(',');
+            var codes = split.ToIntArray();
+            var endFor = false;
+
+            for (var position = 0; position < codes.Length;)
+            {
+                switch (codes[position])
+                {
+                    case 99:
+                        endFor = true;
+                        break;
+                    case 1:
+                        DoAddition(codes, position);
+                        position += 4;
+                        break;
+                    case 2:
+                        DoMultiplication(codes, position);
+                        position += 4;
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+
+                if (endFor)
+                {
+                    break;
+                }
+            }
+
+            return FormatOutput(codes);
+        }
+
+        private static string FormatOutput(int[] codes)
+        {
+            var ret = codes.Aggregate("", (current, code) => $"{current}{code},");
+            return ret.Substring(0, ret.Length - 1);
+        }
+
+        public static string RestoreGravityAssistProgramToAlarm(string input)
+        {
+            var split = input.Split(',').ToIntArray();
+            split[1] = 12;
+            split[2] = 2;
+
+            return FormatOutput(split);
+        }
+
+        private static void DoMultiplication(int[] codes, int position)
+        {
+            var addPosition1 = codes[position + 1];
+            var addPosition2 = codes[position + 2];
+            var outPosition = codes[position + 3];
+            codes[outPosition] = codes[addPosition1] * codes[addPosition2];
+        }
+
+        private static void DoAddition(int[] codes, int position)
+        {
+            var addPosition1 = codes[position + 1];
+            var addPosition2 = codes[position + 2];
+            var outPosition = codes[position + 3];
+            codes[outPosition] = codes[addPosition1] + codes[addPosition2];
+        }
+    }
+}
---
+++ AoCTests/Day2Tests.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode2019.Day2;
+using Xunit;
+
+namespace AoCTests
+{
+    public class Day2Tests
+    {
+        [Theory]
+        [InlineData("1,0,0,0,99", "2,0,0,0,99")]
+        [InlineData("2,3,0,3,99", "2,3,0,6,99")]
+        [InlineData("2,4,4,5,99,0", "2,4,4,5,99,9801")]
+        [InlineData("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")]
+        [InlineData("1,9,10,3,2,3,11,0,99,30,40,50", "3500,9,10,70,2,3,11,0,99,30,40,50")]
+        public void TestOpCode(string input, string expected)
+        {
+            var result = Day2Part1.ProcessCode(input);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
